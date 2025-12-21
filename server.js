44352cb779f1053ac231a40689b1a7aa99c89478
komiiiiii <94881865+komiiiiii@users.noreply.github.com>
2025-12-22 00:04:08 +0500

    Update server.js

--- server.js
+++ server.js
@@ -43,6 +43,53 @@ app.post("/movies", (req, res) => {
   res.status(201).json(newMovie);
 });
 
+app.put("/movies/:id", (req, res) => {
+  const { name } = req.body;
+
+  if (!name) {
+    return res.status(400).json({ message: "Name is required" });
+  }
+
+  const data = readData();
+  const movieId = parseInt(req.params.id);
+
+  const movie = data.movies.find(m => m.id === movieId);
+
+  if (!movie) {
+    return res.status(404).json({ message: "Movie not found" });
+  }
+
+  movie.name = name;
+
+  fs.writeFileSync(
+    path.join(__dirname, "data.json"),
+    JSON.stringify(data, null, 2)
+  );
+
+  res.json(movie);
+});
+
+app.delete("/movies/:id", (req, res) => {
+  const data = readData();
+  const movieId = parseInt(req.params.id);
+
+  const index = data.movies.findIndex(m => m.id === movieId);
+
+  if (index === -1) {
+    return res.status(404).json({ message: "Movie not found" });
+  }
+
+  data.movies.splice(index, 1);
+
+  fs.writeFileSync(
+    path.join(__dirname, "data.json"),
+    JSON.stringify(data, null, 2)
+  );
+
+  res.json({ success: true });
+});
+
+
 app.listen(PORT, () => {
   console.log("Server running on port 3000");
 });
